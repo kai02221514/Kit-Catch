e8438c8d4841bbcf93fe838ff3111b4c6c10a376
北代櫂 <kitadaikai@kitadaikainoMacBook-ea.local>
2022-03-12 21:14:23 +0900

    メニューボタンの開閉を実装

--- components/MenuButton/MenuButton.tsx
+++ components/MenuButton/MenuButton.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
-const BlackBack = styled.div`
+const BlackBack = styled.div<{ open: boolean }>`
+  display: ${({ open }) => (open ? 'block' : 'none')};
   position: absolute;
   left: 0;
   top: 0;
@@ -11,41 +13,77 @@ const BlackBack = styled.div`
   z-index: 2;
 `;
 
-const MenuButtonContainer = styled.div`
-    position: absolute;
-    left: 0;
-    top: 0;
-    z-index: 3;
+const MenuButtonContainer = styled.div<{ open: boolean }>`
+  position: absolute;
+  left: ${({ open }) => (open ? '0' : '-100vw')};
+  transition: left 0.8s;
+  top: 0;
+  z-index: 3;
 `;
 
 const MenuButtonItem = styled.div`
-    &:hover {
-        background-color: #abcbf3;
-    }
-    transition: background-color 1s;
-    background-color: white;
-    display: table-cell;
-    vertical-align: middle;
-    text-align: center;
-    font-weight: bolder;
-    font-size: 2vw;
-    color: black;
-    position: relative;
-    width: 10vw;
-    height: 8vh;
-    border-right: solid lightgray 0.5px;
+  &:hover {
+    background-color: #abcbf3;
+    cursor: pointer;
+  }
+  transition: background-color 1s;
+  background-color: white;
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 2vw;
+  color: black;
+  position: relative;
+  width: 10vw;
+  height: 8vh;
+  border-right: solid lightgray 0.5px;
+`;
+
+const OpenButton = styled.div<{ open: boolean }>`
+  position: fixed;
+  top: 0;
+  left: ${({ open }) => (open ? '48vw' : '0')};
+  transition: left 0.9s;
+  height: 54px;
+  z-index: 3;
+  padding: 6px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div<{ open: boolean }>`
+  transform: rotate(${({ open }) => (open ? '90deg' : '0')});
+  transition: transform 2s;
+  border-top: solid lightgray ${({ open }) => (open ? '25px' : '5px')};
+  border-radius: 5px;
+  margin-top: 10px;
+  padding-left: 60px;
+  &:first-of-type {
+    margin-top: 5px;
+  }
 `;
 
 export function MenuButton() {
-    const menu: string[] = ["時間割", "課題", "点数計算", "LINE連携", "設定"]
-    return (
-        <>
-        <BlackBack />
-        <MenuButtonContainer>
-        {menu.map((key) =>
-            <MenuButtonItem>{key}</MenuButtonItem>
-        )}
-        </MenuButtonContainer>
-        </>
-    )
+  const items: string[] = ['時間割', '課題', '点数計算', 'LINE連携', '設定'];
+  const [menu, setOpen] = useState<boolean>(false);
+  const open = () => {
+    if (menu) setOpen(false);
+    else setOpen(true);
+  };
+  return (
+    <>
+      <OpenButton open={menu} onClick={open}>
+        <Bar open={menu}></Bar>
+        <Bar open={menu}></Bar>
+        <Bar open={menu}></Bar>
+      </OpenButton>
+      <BlackBack open={menu} />
+      <MenuButtonContainer open={menu}>
+        {items.map((key) => (
+          // eslint-disable-next-line react/jsx-key
+          <MenuButtonItem>{key}</MenuButtonItem>
+        ))}
+      </MenuButtonContainer>
+    </>
+  );
 }
