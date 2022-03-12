f835d7055a55068fd4c0405e02f002b040ab1fb0
北代櫂 <kitadaikai@kitadaikainoMacBook-ea.local>
2022-03-12 19:13:47 +0900

    コンフリクトを解消

--- domains/Table/Table.tsx
+++ domains/Table/Table.tsx
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import React from "react";
-import { MenuButton } from "../../components/MenuButton";
-import { TableContainer } from "./TableContainer";
-
-export function Table() {
-    return (
-        <>
-            <MenuButton />
-            <TableContainer />
-        </>
-    )
-}
-=======
 import React from 'react';
+import { MenuButton } from '../../components/MenuButton';
 import { TableContainer } from './TableContainer';
 
 export function Table() {
-  return <TableContainer />;
+  return (
+    <>
+      <MenuButton />
+      <TableContainer />
+    </>
+  );
 }
->>>>>>> ab60ac0470e2aa033108c39f93b4458f07ce42bb
