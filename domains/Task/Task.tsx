eb17de91495e9be728ad2b6067068aa141f558f0
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2022-03-20 18:29:45 +0900

    課題ページを作成

---
+++ domains/Task/Task.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MenuButton } from '../../components/MenuButton';
+import { TaskContainer } from './TaskContainer';
+
+export function Task() {
+  return (
+    <>
+      <MenuButton />
+      <TaskContainer />
+    </>
+  );
+}
